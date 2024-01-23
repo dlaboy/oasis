dc9d23f905a8c79c4b5497d29ed0fac948dc36ae
dlaboy <diego.laboy1@upr.edu>
2024-01-23 15:57:33 -0400

    heroku setup

--- oasis_pos/client/vite.config.js
+++ oasis_pos/client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    base: '/oasispos/',
     port:5173,
     proxy: {
       '/orders': {
