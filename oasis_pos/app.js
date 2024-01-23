66cc346d843af4e858f618afe28eae1de9c4ca6e
dlaboy <diego.laboy1@upr.edu>
2024-01-23 17:16:33 -0400

    changes

--- oasis_pos/app.js
+++ oasis_pos/app.js
@@ -58,7 +58,7 @@ app.use(cors(corsOptions));
 if (process.env.NODE_ENV === "production"){
   app.use(express.static("client/public"))
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build" ,"index.html"));
   })
 }
 
