fa0f7833e1ba4665028c84ba11fc8fa9bded9f3f
dlaboy <diego.laboy1@upr.edu>
2024-04-20 08:35:49 -0400

    fixed bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig, preprocessCSS } from 'vite'
 import react from '@vitejs/plugin-react'
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
 
 dotenv.config();
 var NODE_ENV = process.env.NODE_ENV;
 var URL;
 
-if (NODE_ENV != None){
-  if (NODE_ENV == production){
+if (typeof NODE_ENV != "undefined"){
+  if (NODE_ENV == 'production'){
     URL = 'https://oasispos-6173005c2083.herokuapp.com/orders'
   }
   else{
