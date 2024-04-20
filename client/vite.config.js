05698fc7f46385bf945719f2c721b377a19e523e
dlaboy <diego.laboy1@upr.edu>
2024-04-20 04:31:25 -0400

    got to do an production-like development environment!

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/orders': {
-        target: 'https://oasispos-6173005c2083.herokuapp.com/orders', // Your API server addressx
-        // target: 'http://127.0.0.1:3000/orders', // Your API server address
+        // target: 'https://oasispos-6173005c2083.herokuapp.com/orders', // Your API server addressx
+        target: 'http://127.0.0.1:3000/orders', // Your API server address
         changeOrigin: true,
         rewrite: (path) => path.replace('/orders', ''),
       },
