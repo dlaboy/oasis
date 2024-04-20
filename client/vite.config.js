59340c5185c529856c7e0da62fb6a5e4af0fac06
dlaboy <diego.laboy1@upr.edu>
2024-04-20 08:23:31 -0400

    dynamic proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, preprocessCSS } from 'vite'
 import react from '@vitejs/plugin-react'
+const dotenv = require('dotenv');
+
+var development = process.env.development;
+var URL;
+
+if (development != ""){
+  if (development == True){
+    URL = 'http://127.0.0.1:3000/orders'
+  }
+  else{
+    URL = 'https://oasispos-6173005c2083.herokuapp.com/orders'
+  }
+
+}
+
+
+dotenv.config();
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/orders': {
-        // target: 'https://oasispos-6173005c2083.herokuapp.com/orders', // Your API server addressx
-        target: 'http://127.0.0.1:3000/orders', // Your API server address
+        target: URL, // Your API server address
         changeOrigin: true,
         rewrite: (path) => path.replace('/orders', ''),
       },
