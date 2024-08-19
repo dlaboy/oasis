e9541381140279c5821121a00cddb1dcafbf871e
dlaboy <diego.laboy1@upr.edu>
2024-08-19 19:27:50 -0400

    search pattern

--- server/routes/ingredientes.js
+++ server/routes/ingredientes.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var mongoose = require("mongoose");
 var router = express.Router();
-var Ingrediente = require('../models/ingredientes.js')
+var Ingrediente = require('../models/ingredientes.js');
 var db = require("../db.js");
-
 
 // /* GET users listing. */
 router.get('/', async function (req, res, next) {
-    console.log("Cuerpo del ingrediente que llegó al backend", req.query)
-    if (req.query.nombre != undefined){
-        var ingredientes = await Ingrediente.find({nombre: req.query.nombre})
-
-        console.log("Ingredientes buscados desde el backend",ingredientes)
-    }
-    else{
-        var ingredientes = await Ingrediente.find({})
-
-        console.log("Ninguna busqueda",ingredientes)
-
+    console.log("Cuerpo del ingrediente que llegó al backend", req.query);
+    
+    let ingredientes;
+    if (req.query.nombre != undefined) {
+        // Use a regular expression to find all ingredients that start with the given string
+        const searchPattern = new RegExp('^' + req.query.nombre, 'i');
+        ingredientes = await Ingrediente.find({ nombre: { $regex: searchPattern } });
+        
+        console.log("Ingredientes buscados desde el backend", ingredientes);
+    } else {
+        ingredientes = await Ingrediente.find({});
+        
+        console.log("Ninguna busqueda", ingredientes);
     }
     
     res.send(ingredientes);
--- server/routes/toppings.js
+++ server/routes/toppings.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var mongoose = require("mongoose");
 var router = express.Router();
-var Toppings = require('../models/toppings.js')
+var Toppings = require('../models/toppings.js');
 var db = require("../db.js");
-
 
 // /* GET users listing. */
 router.get('/', async function (req, res, next) {
-    console.log("Cuerpo del ingrediente que llegó al backend", req.query)
-    if (req.query.nombre != undefined){
-        var toppings = await Toppings.find({nombre: req.query.nombre})
-
-        console.log("Toppingss buscados desde el backend",toppings)
-    }
-    else{
-        var toppings = await Toppings.find({})
-
-        console.log("Ninguna busqueda",toppings)
-
+    console.log("Cuerpo del ingrediente que llegó al backend", req.query);
+    
+    let toppings;
+    if (req.query.nombre != undefined) {
+        // Use a regular expression to find all toppings that start with the given string
+        const searchPattern = new RegExp('^' + req.query.nombre, 'i');
+        toppings = await Toppings.find({ nombre: { $regex: searchPattern } });
+        
+        console.log("Toppings buscados desde el backend", toppings);
+    } else {
+        toppings = await Toppings.find({});
+        
+        console.log("Ninguna busqueda", toppings);
     }
     
     res.send(toppings);
