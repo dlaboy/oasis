71b8fe36eedf29b6a11770925d3ad4bbc3b08ec0
dlaboy <diego.laboy1@upr.edu>
2024-08-19 19:11:57 -0400

    favorites

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var mongoose = require("mongoose");
 var router = express.Router();
-var Favorites = require('../models/favorites.js')
+var Favorites = require('../models/favorites.js');
 var db = require("../db.js");
-
 
 // /* GET users listing. */
 router.get('/', async function (req, res, next) {
-    console.log("Cuerpo del ingrediente que llegó al backend", req.query)
-    if (req.query.nombre != undefined){
-        var favorites = await Favorites.find({nombre: req.query.nombre})
-
-        console.log("Favoritess buscados desde el backend",favorites)
-    }
-    else{
-        var favorites = await Favorites.find({})
-
-        console.log("Ninguna busqueda",favorites)
-
+    console.log("Cuerpo del ingrediente que llegó al backend", req.query);
+    
+    let favorites;
+    if (req.query.nombre != undefined) {
+        // Use a regular expression to find all favorites that start with the given string
+        const searchPattern = new RegExp('^' + req.query.nombre, 'i');
+        favorites = await Favorites.find({ nombre: { $regex: searchPattern } });
+        
+        console.log("Favorites buscados desde el backend", favorites);
+    } else {
+        favorites = await Favorites.find({});
+        
+        console.log("Ninguna busqueda", favorites);
     }
     
     res.send(favorites);
